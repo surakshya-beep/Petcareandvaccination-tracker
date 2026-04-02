c88150a687f64dd8f96d7fb2be486f45a77227c5
Surakshya-beep <surakshyakhatri@35gmail.com>
2026-04-02 16:50:18 +0545

    Add render.yaml and fix dynamic Render port

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ app.post('/login', async (req, res) => {
 });
 
 // Start Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
